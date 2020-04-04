117f9379e85e1956a9fe56940dadcf45ac96b62b
Sia <ashkiani@yahoo.com>
2020-04-04 10:53:17 -0400

    Added a UC component to show a UNDER CONSTRUCTION message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import UC from "./components/UC";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      UNDER CONSTRUCTION
+      <UC />
       <Footer />
     </div>
   );
---
+++ src/components/UC.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function UC() {
+    return (
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">Under Construction</h1>
+                <p class="lead">This is app is under construction. Check back soon.</p>
+            </div>
+        </div>
+    )
+}
