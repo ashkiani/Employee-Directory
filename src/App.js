de21b1aecff424f5402614e2704277bfabcff762
Sia <ashkiani@yahoo.com>
2020-04-04 11:07:25 -0400

    Added a Header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import UC from "./components/UC";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <UC />
+      <Header />
       <Footer />
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header class="">
+            <div class="jumbotron jumbotron bg-faded justify-content-center bg-info text-white my-2">
+                <div class="container">
+                    <h1>Employee Directory</h1>
+                </div>
+            </div>
+        </header>
+    )
+}
