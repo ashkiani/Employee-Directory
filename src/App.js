d7f5d189f44d45badb80d1bb4108de21b49c6c40
Sia <ashkiani@yahoo.com>
2020-04-04 10:49:05 -0400

    Added a Footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       UNDER CONSTRUCTION
+      <Footer />
     </div>
   );
 }
