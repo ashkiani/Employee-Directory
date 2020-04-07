35f666ac1539f5cbc8107e06b4502e0c999722a3
Sia <ashkiani@yahoo.com>
2020-04-07 09:53:17 -0400

    Updated the Employees component to add Image, email, phone etc...in the table

--- src/components/Employees.js
+++ src/components/Employees.js
@@ -13,14 +13,29 @@ export default function Employees() {
     }, []); // <-- Have to pass in [] here!
 
     return (
-        <div className="datatable mt-5">
-            <table id="table" className="table table-striped table-hover table-condensed">
-                <thead>
-                    {users.map(user => (
-                        <tr key={user.login.uuid.toString()}><th>{`${user.name.first} ${user.name.last}`}</th></tr>
-                    ))}
-                </thead>
-            </table>
-        </div>
+        <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Image</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Phone</th>
+                </tr>
+            </thead>
+            <tbody>
+                {users.map((user,index) => (
+                    <tr key={user.login.uuid.toString()}>
+                        <th scope="row">{index+1}</th>
+                        <td><img src={user.picture.thumbnail} alt="Smiley face" height="42" width="42"></img></td>
+                        <td>{user.name.first}</td>
+                        <td>{user.name.last}</td>
+                        <td>{user.email}</td>
+                        <td>{user.phone}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
     );
 }
