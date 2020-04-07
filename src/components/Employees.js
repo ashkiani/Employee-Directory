969bcf5b05f8bd3963bb792032f28c5cab657092
Sia <ashkiani@yahoo.com>
2020-04-06 21:15:02 -0400

    Added hooks for the Employee component to load the returned API data

--- src/components/Employees.js
+++ src/components/Employees.js
@@ -1,21 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import API from '../utils/API';
 
 export default function Employees() {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        API.getUsers()
+            .then(results => results.json())
+            .then(data => {
+                setUsers(data.results);
+
+            });
+    }, []); // <-- Have to pass in [] here!
+
     return (
         <div className="datatable mt-5">
-            <table
-                id="table"
-                className="table table-striped table-hover table-condensed"
-            >
+            <table id="table" className="table table-striped table-hover table-condensed">
                 <thead>
-                    <tr>
-                    <th>Employee 1</th>
-                    </tr>
-                    <tr>
-                    <th>Employee 2</th>
-                    </tr>
+                    {users.map(user => (
+                        <tr key={user.login.uuid.toString()}><th>{`${user.name.first} ${user.name.last}`}</th></tr>
+                    ))}
                 </thead>
             </table>
         </div>
-    )
+    );
 }
