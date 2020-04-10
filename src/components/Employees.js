b7e49dfde9fbe8e4f9f462b90507c65d70368511
Sia <ashkiani@yahoo.com>
2020-04-10 09:01:12 -0400

    Added a Sort Name feature

--- src/components/Employees.js
+++ src/components/Employees.js
@@ -3,14 +3,24 @@ import API from '../utils/API';
 import Employee from "./Employee";
 
 export default function Employees() {
-    const [users, setUsers] = useState([]);
-    const [filteredUsers, setFilteredUsers] = useState([]);
+    const [users, setUsers] = useState({ users: [], sort: 0 });
+    const [filteredUsers, setFilteredUsers] = useState({ users: [], sort: 0 });
     const [search, setSearch] = useState("");
 
+
     useEffect(() => {
+        API.getUsers()
+            .then(results => results.json())
+            .then(data => {
+                setUsers({ users: data.results, sort: 0 });
+            });
+    }, []); // <-- Have to pass in [] here!
+
+    useEffect(() => {
+        console.log("useEffect for search, users, sort")
         let filUsers = [];
         if (search !== "") {
-            filUsers = users.filter(user => { 
+            filUsers = users.filter(user => {
                 let lowerFirst = user.name.first.toLowerCase();
                 let lowerLast = user.name.last.toLowerCase();
                 let lowerSearch = search.toLowerCase();
@@ -18,23 +28,28 @@ export default function Employees() {
                 let nameHit = (lowerFirst.includes(lowerSearch) || lowerLast.includes(lowerSearch));
                 let emailHit = lowerEmail.includes(lowerSearch);
                 let phoneHit = user.phone.includes(lowerSearch);
-                return (nameHit || emailHit || phoneHit )});
+                return (nameHit || emailHit || phoneHit)
+            });
         }
         else {
-            filUsers = users;
+            filUsers = users.users;
         }
-        setFilteredUsers(filUsers);
+        switch (users.sort) {
+            case 1:
+                filUsers.sort((a, b) => (a.name.first > b.name.first) ? 1 : (a.name.first === b.name.first) ? ((a.name.last > b.name.last) ? 1 : -1) : -1)
+                break;
+            case -1:
+                filUsers.sort((a, b) => (a.name.first > b.name.first) ? -1 : (a.name.first === b.name.first) ? ((a.name.last > b.name.last) ? -1 : 1) : 1)
+                break;
+            default:
+                break;
+        }
+        setFilteredUsers({ users: filUsers, sort: users.sort });
     }, [search, users]);
 
-    useEffect(() => {
-        API.getUsers()
-            .then(results => results.json())
-            .then(data => {
-                setUsers(data.results);
-
-            });
-    }, []); // <-- Have to pass in [] here!
-
+    function sort(e, order) {
+        setUsers({ users: filteredUsers.users, sort: order })
+    }
     return (
         <>
             <div className="row">
@@ -54,6 +69,17 @@ export default function Employees() {
 
                 </div>
             </div>
+            <div className="row justify-content-center">
+                <div >Sort Names</div>
+                <div className="btn-group btn-group-toggle ml-3 mb-3" data-toggle="buttons">
+                    <label className="btn btn-primary">
+                        <input type="radio" name="options" id="option2" onClick={e => sort(e, 1)} checked={filteredUsers.sort === 1} onChange={e=>console.log("")} /><i className="fas fa-sort-alpha-down"></i>
+                    </label>
+                    <label className="btn btn-primary">
+                        <input type="radio" name="options" id="option3" onClick={e => sort(e, -1)} /><i className='fas fa-sort-alpha-up-alt'></i>
+                    </label>
+                </div>
+            </div>
             <table className="table">
                 <thead className="thead-dark">
                     <tr>
@@ -66,7 +92,7 @@ export default function Employees() {
                     </tr>
                 </thead>
                 <tbody>
-                    {filteredUsers.map((user, index) => (
+                    {filteredUsers.users.map((user, index) => (
                         <Employee key={user.login.uuid.toString()} data={{ "index": index, "user": user }} />
                     ))}
                 </tbody>
